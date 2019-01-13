1cb95202cc7428e3453cae7e86cbbfb0591d30eb
Jeffrey Tan <jeffrey.tan712@gmail.com>
2019-01-13 00:39:32 -0600

    Finished the Zombie Mod.

--- src/main/java/minecraftbyexample/mbe100_zombie_mod/ZombieHurtsVillagerEventHandler.java
+++ src/main/java/minecraftbyexample/mbe100_zombie_mod/ZombieHurtsVillagerEventHandler.java
@@ -1,8 +1,11 @@
 package minecraftbyexample.mbe100_zombie_mod;
 
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -16,7 +19,20 @@ public class ZombieHurtsVillagerEventHandler {
   public void onLivingHurtEvent(LivingHurtEvent event)
   {
     if (event.getEntity() instanceof EntityVillager) {
-      System.out.println("PENGUIN");
+
+      if (event.getSource().getTrueSource() instanceof EntityZombie) {
+
+        EntityZombie entityZombie = new EntityZombie(event.getEntity().getEntityWorld());
+        entityZombie.setLocationAndAngles(event.getEntity().posX, event.getEntity().posY, event.getEntity().posZ, event.getEntity().rotationYaw, event.getEntity().rotationPitch);
+
+        if (event.getEntity().hasCustomName()) {
+          entityZombie.setCustomNameTag(event.getEntity().getCustomNameTag());
+          entityZombie.setAlwaysRenderNameTag(event.getEntity().getAlwaysRenderNameTag());
+        }
+
+        event.getEntity().getEntityWorld().spawnEntity(entityZombie);
+        event.getEntity().setDead();
+      }
     }
   }
 }
